d7e50ed3c0915d4b9df53e278d288d8922d7cc61
Alex-Costa <ass_costa@hotmail.com>
2021-03-25 13:57:03 -0300

    Criação de Entidades
    - Pessoa_Beneficio
    - Pessoa_Servico

---
+++ gestorpublico.negocio/src/main/java/org/gestorpublico/entidade/Pessoa_Beneficio.java
@@ -0,0 +1,95 @@
+package org.gestorpublico.entidade;
+
+import lombok.*;
+import org.gestorpublico.util.CassUtil;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(
+    indexes = {
+            @Index(columnList = "dh_cadastro", name = "dataHoraCadastro"),
+            @Index(columnList = "dt_concessao", name = "dataConcessao")
+    }
+)
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode @ToString
+public class Pessoa_Beneficio implements Serializable, Comparable<Pessoa_Beneficio> {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "dh_cadastro", nullable = false)
+    private LocalDateTime dataHoraCadastro = LocalDateTime.now();
+
+    @Column(name = "dt_concessao", nullable = false)
+    private LocalDate dataConcessao;
+
+    @Column(name = "nu_valorUnitario", nullable = false, columnDefinition = "decimal(10,2)")
+    private BigDecimal valorUnitario;
+
+    @Column(name = "in_quantidade", nullable = false)
+    private Integer quantidade;
+
+    // **************************** RELACIONAMENTOS *************************
+    @ManyToOne
+    @JoinColumn(name="fk_responsavel", nullable=false, foreignKey=@ForeignKey(name="FK_Pessoa_Pessoa_Beneficio_responsavel"))
+    private Pessoa responsavel;
+
+    @ManyToOne
+    @JoinColumn(name="fk_beneficiado", nullable=false, foreignKey=@ForeignKey(name="FK_Pessoa_Pessoa_Beneficio_beneficiado"))
+    private Pessoa beneficiado;
+
+    @ManyToOne
+    @JoinColumn(name="fk_beneficio", foreignKey=@ForeignKey(name="FK_Beneficio_Pessoa_Endereco"))
+    private Beneficio beneficio;
+
+    // **************************** CONTRUTORES *****************************
+
+    // ****************** HASH, EQUALS, COMPARETO, TOSTRING *****************
+    @Override
+    public int compareTo(Pessoa_Beneficio o) {
+        int compare = this.beneficiado.compareTo(o.getBeneficiado());
+        compare = compare != 0 ? compare : beneficio.compareTo(o.getBeneficio());
+        compare = compare != 0 ? compare : dataConcessao.compareTo(o.getDataConcessao());
+        return compare != 0 ? compare : dataHoraCadastro.compareTo(o.getDataHoraCadastro());
+    }
+
+    // ****************** GETs e SETs ***************************************
+    public String getDataHoraCadastroFormatada() {
+        return CassUtil.getDataHoraFormatada(dataHoraCadastro);
+    }
+
+    public String getValorUnitarioFormatado() {
+        return CassUtil.converterBigDecimalParaNumeroStringPtBr(valorUnitario);
+    }
+
+    public void setValorUnitario(String valorUnitario) {
+        this.valorUnitario = CassUtil.converterNumeroStringPtBrParaBigDecimal(valorUnitario);
+    }
+
+    /**
+     * Calcula o valor total do benefício multiplicando o valor unitário pela quantidade
+     * @return BigDecimal
+     */
+    public BigDecimal getValorTotal() {
+        return valorUnitario.multiply(new BigDecimal(quantidade));
+    }
+
+    /**
+     * Calcula o valor total do benefício multiplicando o valor unitário pela quantidade
+     * @return String
+     */
+    public String getValorTotalFormatado() {
+        return CassUtil.converterBigDecimalParaNumeroStringPtBr(valorUnitario.multiply(new BigDecimal(quantidade)));
+    }
+}
---
+++ gestorpublico.negocio/src/main/java/org/gestorpublico/entidade/Pessoa_Servico.java
@@ -0,0 +1,92 @@
+package org.gestorpublico.entidade;
+
+import lombok.*;
+import org.gestorpublico.util.CassUtil;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(
+    indexes = {
+            @Index(columnList = "dh_cadastro", name = "dataHoraCadastro"),
+            @Index(columnList = "dt_inicio", name = "inicio"),
+            @Index(columnList = "dt_termino", name = "termino")
+    }
+)
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode @ToString
+public class Pessoa_Servico implements Serializable, Comparable<Pessoa_Servico> {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "dh_cadastro", nullable = false)
+    private LocalDateTime dataHoraCadastro = LocalDateTime.now();
+
+    @Column(name = "dt_inicio")
+    private LocalDate dataInicio;
+
+    @Column(name = "dt_termino")
+    private LocalDate dataTermino;
+
+    @Column(name = "nu_valorInicial", nullable = false, columnDefinition = "decimal(10,2) default 0.00")
+    private BigDecimal valorInicial;
+
+    @Column(name = "nu_valorFinal", columnDefinition = "decimal(10,2)")
+    private BigDecimal valorFinal;
+
+    @Column(name = "nm_observacao")
+    private String observacao;
+
+    // **************************** RELACIONAMENTOS *************************
+    @ManyToOne
+    @JoinColumn(name="fk_solicitante", nullable=false, foreignKey=@ForeignKey(name="FK_Pessoa_Pessoa_Servico"))
+    private Pessoa solicitante;
+
+    @ManyToOne
+    @JoinColumn(name="fk_servico", foreignKey=@ForeignKey(name="FK_Servico_Pessoa_Servico"))
+    private Servico servico;
+
+    // **************************** CONTRUTORES *****************************
+
+    // ****************** HASH, EQUALS, COMPARETO, TOSTRING *****************
+    @Override
+    public int compareTo(Pessoa_Servico o) {
+        int compare = this.solicitante.compareTo(o.getSolicitante());
+        compare = compare != 0 ? compare : servico.compareTo(o.getServico());
+        return compare != 0 ? compare : getDataHoraCadastro().compareTo(o.getDataHoraCadastro());
+    }
+
+    // ****************** GETs e SETs ***************************************
+    public String getDataHoraCadastroFormatada() {
+        return CassUtil.getDataHoraFormatada(dataHoraCadastro);
+    }
+
+    public String getValorInicialFormatado() {
+        return CassUtil.converterBigDecimalParaNumeroStringPtBr(valorInicial);
+    }
+
+    public void setValorInicial(String valorInicial) {
+        this.valorInicial = CassUtil.converterNumeroStringPtBrParaBigDecimal(valorInicial);
+    }
+
+    public String getValorFinalFormatado() {
+        return CassUtil.converterBigDecimalParaNumeroStringPtBr(valorFinal);
+    }
+
+    public void setValorFinal(String valorFinal) {
+        this.valorFinal = CassUtil.converterNumeroStringPtBrParaBigDecimal(valorFinal);
+    }
+
+    public void setObservacao(String observacao) {
+        this.observacao = observacao == null || observacao.trim().isEmpty() ? null : observacao.trim();
+    }
+}
