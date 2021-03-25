a2c4f2c5d1a4c8388e5ac8b74fc757c06a3c1ec5
Alex-Costa <ass_costa@hotmail.com>
2021-03-25 08:16:26 -0300

    Criação de classes
    Criação das entidades:
    - Poder_Setor_Pessoa
    - Benefficio

--- gestorpublico.negocio/src/main/java/org/gestorpublico/entidade/Beneficio.java
+++ gestorpublico.negocio/src/main/java/org/gestorpublico/entidade/Beneficio.java
@@ -2,23 +2,26 @@ package org.gestorpublico.entidade;
 
 import lombok.*;
 import org.gestorpublico.util.CassUtil;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
 @Table(
+    uniqueConstraints = {
+        @UniqueConstraint(name="nm_nome", columnNames="nm_nome")
+    },
     indexes = {
-        @Index(columnList = "dt_alocacao", name = "dataAlocacao"),
-        @Index(columnList = "dt_desalocacao", name = "dataDesalocacao"),
-        @Index(columnList = "bl_acumulo", name = "acumulo")
+        @Index(columnList = "nu_valor", name = "nu_valor")
     }
 )
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 @EqualsAndHashCode @ToString
-public class Poder_Setor_Pessoa implements Serializable, Comparable<Poder_Setor_Pessoa> {
+public class Beneficio implements Serializable, Comparable<Beneficio> {
 
     private static final long serialVersionUID = 1L;
 
@@ -26,36 +29,28 @@ public class Poder_Setor_Pessoa implements Serializable, Comparable<Poder_Setor_
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "dt_alocacao", nullable = false)
-    private LocalDate dataAlocacao;
+    @Column(name = "nm_nome", nullable = false)
+    private String nome;
 
-    @Column(name = "dt_desalocacao")
-    private LocalDate dataDesalocacao;
+    @Type(type = "text")
+    @Column(name = "tx_descricao")
+    private String descricao;
 
-    @Column(name = "bl_acumulo", nullable = false, columnDefinition = "tinyint(1) default 0")
-    private boolean acumulo;
+    @Column(name = "nu_valor", nullable = false, columnDefinition = "decimal(10,2) default 0.00")
+    private BigDecimal valor;
 
     // **************************** RELACIONAMENTOS *************************
     @ManyToOne
     @JoinColumn(name="fk_poderSetor", nullable=false, foreignKey=@ForeignKey(name="FK_Poder_Setor_Poder_Setor_Pessoa"))
     private Poder_Setor poderSetor;
 
-    @ManyToOne
-    @JoinColumn(name="fk_pessoa", nullable=false, foreignKey=@ForeignKey(name="FK_Pessoa_Poder_Setor_Pessoa"))
-    private Pessoa pessoa;
-
-    @ManyToOne
-    @JoinColumn(name="fk_funcao", nullable = false, foreignKey=@ForeignKey(name="FK_Funcao_Poder_Setor_Pessoa"))
-    private Funcao funcao;
-
     // **************************** CONTRUTORES *****************************
 
     // ****************** HASH, EQUALS, COMPARETO, TOSTRING *****************
     @Override
-    public int compareTo(Poder_Setor_Pessoa o) {
+    public int compareTo(Beneficio o) {
         int compare = this.poderSetor.getId().compareTo(o.getPoderSetor().getId());
-        compare = compare != 0 ? compare : pessoa.getNome().compareTo(o.getPessoa().getNome());
-        return compare != 0 ? compare : funcao.getNome().compareTo(o.getFuncao().getNome());
+        return compare != 0 ? compare : nome.compareTo(o.getFuncao().getNome());
     }
 
     // ****************** GETs e SETs ***************************************
